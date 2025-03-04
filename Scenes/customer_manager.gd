43e641cb18882fd6c701cb967fba4347f7a7938b
rahulCh022006 <challapallirahul12@gmail.com>
2025-03-04 19:35:57 +0530

    feat: Implemented randomized orders and menu
    - Added a system to randomize customer orders using predefined pani puri combinations.
    
    - Orders are now assigned dynamically when a customer spawns.
    
    - Each customer gets a new order after being served.
    
    - Implemented a simple UI menu allowing players to start, restart, or exit the game.
    
    - Added button functionality for navigation.
    
    - Ensured smooth transitions between menu and gameplay.

--- Scenes/customer_manager.gd
+++ Scenes/customer_manager.gd
@@ -1,8 +1,25 @@
 extends Area2D
 
 @export var ingredient_handler: NodePath
+@onready var label: Label = $"../Speech/Label"
 
-var order: Array = ["Puri", "Chole", "Pani"]
+#Different Puris
+var different_puris : Dictionary = {
+	"Mitha Puri": ["Puri","Chole","Mitha"],
+	"Mix\nPuri": ["Puri","Chole","Mitha","Pani"],
+	"Theeka Puri": ["Puri","Chole","Pani"]
+}
+#OrderCreation
+var active_order: Array
+	
+func randomize_order():
+	var random_name = different_puris.keys()[randi()%different_puris.size()]
+	active_order = different_puris[random_name]
+	print(active_order)
+	label.text = random_name
+
+func _ready() -> void:
+	randomize_order()
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouse and event.is_pressed():
@@ -11,9 +28,10 @@ func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void
 			check_order(ingredient_handler_node.active_puri, ingredient_handler_node)
 
 func check_order(active_puri, ingredient_handler_node):
-	if active_puri == order:
+	if active_puri == active_order:
 		print("Correct Order")
 		ingredient_handler_node.clear_ingredients()
+		randomize_order()
 	else:
 		print("Wrong dummbo")
 		ingredient_handler_node.clear_ingredients()
--- Scenes/game_manager.gd
+++ Scenes/game_manager.gd
@@ -1,17 +1,32 @@
 extends Node
 
 var is_paused : bool
+@onready var ui: CanvasLayer = $"../UI"
 
 
 func _ready() -> void:
-	is_paused = false
+	is_paused = true
+	ui.visible = is_paused
 
 func pause():
 	get_tree().paused = true
 	
-	
+
+
 func restart_():
 	if Input.is_action_pressed("Restart"):
 		get_tree().reload_current_scene()
 	if Input.is_action_pressed("Pause"):
 		is_paused = true
+
+
+func _on_start_pressed() -> void:
+	ui.visible = false
+
+
+func _on_pause_button_pressed() -> void:
+	ui.visible = true
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
